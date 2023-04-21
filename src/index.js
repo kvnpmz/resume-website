a00cc2bc592d2980cce2dba853e4f6ad491f878b
kvnpmz <kvnpmz@protonmail.com>
2023-04-20 22:07:18 -0400

    Tested frontend API request

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
+import { useEffect, useState } from "react";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import ResumeWebsite from './firstPage/resumeWebsite';
-import PhrasesApp from './secondPage/phrasesApp';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <ResumeWebsite />,
-  },
-  {
-    path: "phrases-app",
-    element: <PhrasesApp />,
-  },
-]);
+import axios from "axios";
+
+export default function TestRequest() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:5000")
+            .then((res) => {
+                setData(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>{data}</h1>
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <TestRequest />
     </React.StrictMode>
 );
+
